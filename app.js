906d8bbf178db5f16b18c26c3a72b204a5987611
dimah <dima.holubenko@gmail.com>
2019-04-06 01:32:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,28 @@
   };
   firebase.initializeApp(config);
 var firestore = firebase.firestore();
+
+const docRef = firebase.doc("filter/param");
+const outputHeader = document.querySelector("#pokerStatusOutput");
+const inputTextField = document.querySelector("#pokerStatus");
+const saveButton = document.querySelector("#saveStatusButton");
+const loadButton = document.querySelector("#loadStatusButton");
+
+saveButton.addEventListener("click", function(){
+  const newStatus = inputTextField.value;
+  docRef.set({
+    Country : newStatus
+  });
+});
+loadButton.addEventListener("click", function () {
+  docRef.get().then(function (doc) {
+    if(doc && doc.exists){
+      const myData = doc.data();
+      outputHeader.innerText = "Status = " + myData.Country;
+    }
+  }).catch(function (error) {
+    console.log("error");
+  });
+
+});
+});
