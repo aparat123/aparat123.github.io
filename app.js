f40fe7eb2ed84892cf28fe8770f1996928f86515
dimah <dima.holubenko@gmail.com>
2019-04-05 20:40:33 +0300

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAn0idywCGZyCEDf3_VVOrM5-LigXkP4Qc",
+    authDomain: "pokeronlinedom.firebaseapp.com",
+    databaseURL: "https://pokeronlinedom.firebaseio.com",
+    projectId: "pokeronlinedom",
+    storageBucket: "pokeronlinedom.appspot.com",
+    messagingSenderId: "727511636242"
+  };
+  firebase.initializeApp(config);
+var firestore = firebase.firestore();
